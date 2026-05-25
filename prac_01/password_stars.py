217fd01918a5f17147afe6d1075cd212fa9f578e
BennyCha <bennycha99@gmail.com>
2026-05-25 18:54:17 +0800

    Refactor password checker to use functions

--- prac_01/password_stars.py
+++ prac_01/password_stars.py
@@ -1,5 +1,14 @@
 """Password stars"""
+def main():
+    password = get_password()
 
-password = input("Enter a password: ")
+    display_password(password)
 
-print("*" * len(password))
+
+def display_password(password: str):
+    print("*" * len(password))
+
+
+def get_password() -> str:
+    password = input("Enter a password: ")
+    return password
