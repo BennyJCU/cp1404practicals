37938f9a2a3e115dcafae78e03724a64ed3d40b3
BennyCha <bennycha99@gmail.com>
2026-05-25 19:05:48 +0800

    Refactor Score determine program to use functions

--- prac_01/score.py
+++ prac_01/score.py
@@ -2,13 +2,22 @@
 CP1404/CP5632 - Practical
 Program to determine score status
 """
+def main():
+    score = float(input("Enter score: "))
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+    result = determines_score(score)
+    print(f"User score {score} is {result}")
+
+
+def determines_score(score: float):
+    if score < 0 or score > 100:
+        result = "Invalid score"
+    elif score >= 90:
+        result = "Excellent"
+    elif score >= 50:
+        result = "Passable"
+    else:
+        result = "Bad"
+    return result
+
+main()
